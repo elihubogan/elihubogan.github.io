a9569f3ecd6a8dbc4b14e65aa0e2a570d223d9d4
Elihu MacBookPro <elihubogan@gmail.com>
2014-02-10 22:11:01 -0800

    modal in process, img width fix

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,23 @@
-$(document.links).filter(function() {
+/*$(document.links).filter(function() {
     //return this.hostname != window.location.hostname;
 }).attr('target', '_blank');)))
+*/
+$(document).ready(function(){
+    $('.modalLink').modal({
+        trigger: '.modalLink',
+        olay:'div.overlay',
+        modals:'div.modal',
+        animationEffect: 'fadeIn', // slidedown
+        animationSpeed: 400,
+        moveModalSpeed: 'slow',
+        background: '333',
+        opacity: 0.8,
+        openOnLoad: false,
+        docClose: true,
+        closeByEscape: true,
+        moveOnScroll: true,
+        resizeWindow: true,
+        video:'http://player.vimeo.com/video/9641036?color=eb5a3d',
+        close:'.closeBtn'
+    });
+});
