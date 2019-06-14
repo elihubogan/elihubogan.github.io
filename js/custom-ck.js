fa8b0879214710f868f015463b7b057e769eeb6c
Elihu Bogan <elihubogan@gmail.com>
2019-06-14 11:23:26 -0400

    nuke it

--- js/custom-ck.js
+++
@@ -1,4 +0,0 @@
-/*$(document.links).filter(function() {
-    //return this.hostname != window.location.hostname;
-}).attr('target', '_blank');)))
-*/$(document).ready(function(){$(".modalLink").modal({trigger:".modalLink",olay:"div.overlay",modals:"div.modal",animationEffect:"fadeIn",animationSpeed:400,moveModalSpeed:"slow",background:"333",opacity:.8,openOnLoad:!1,docClose:!0,closeByEscape:!0,moveOnScroll:!0,resizeWindow:!0,video:"http://player.vimeo.com/video/9641036?color=eb5a3d",close:".closeBtn"})});
